c2e514676af125655c2dece286d969d0572614ac
Alex <kasperskiy.94@gmail.com>
2023-06-22 22:30:00 +0300

    Third task

--- script.js
+++ script.js
@@ -9,3 +9,13 @@ let personalMovieDB = {
     genres: [],
     privat: false
 };
+
+const a = prompt('One of the latest watched movies?'),
+      b = prompt('What rank do you give that film?'),
+      c = prompt('One of the latest watched movies?'),
+      d = prompt('What rank do you give that film?');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
