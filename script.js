77e737af1ab0c3bbe429645c3658c55cb579e8a8
Alex <kasperskiy.94@gmail.com>
2023-06-22 22:01:35 +0300

    Second task

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 "use strict"
 
 let numberOfFilms = prompt('How many movies did you watch?');
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
