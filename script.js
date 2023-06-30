bf6eab555790c54e817ef575933e0cf0cffa6c84
Alex <kasperskiy.94@gmail.com>
2023-06-30 14:15:39 +0300

    2. Second task

--- script.js
+++ script.js
@@ -14,7 +14,13 @@ for (let i =0; i < 2; i++) {
     const a = prompt('One of the latest watched movies?', ''),
           b = prompt('What rank do you give that film?', '');
 
-    personalMovieDB.movies[a] = b;
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        i--; //Завдяки декріменту ми повертаємо користувача на крок назад
+    }
 }
 
 console.log(personalMovieDB);
