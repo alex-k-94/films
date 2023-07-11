277c6ff147bad2308bc6b3eab0fc953d274571e2
Alex <kasperskiy.94@gmail.com>
2023-07-11 19:45:17 +0300

    3. First task

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
 "use strict"
 
-let numberOfFilms = prompt('How many movies did you watch?');
+let numberOfFilms;
+
+function start() {
+    numberOfFilms = +prompt('How many movies did you watch?', '');
+
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+        numberOfFilms = +prompt('How many movies did you watch?', '');
+    }
+}
+
+start();
 
 let personalMovieDB = {
     count: numberOfFilms,
@@ -10,27 +20,35 @@ let personalMovieDB = {
     privat: false
 };
 
-for (let i =0; i < 2; i++) {
-    const a = prompt('One of the latest watched movies?', ''),
-          b = prompt('What rank do you give that film?', '');
-
-    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else {
-        console.log('error');
-        i--; //Завдяки декріменту ми повертаємо користувача на крок назад
+function rememberMyFilms() {
+    for (let i =0; i < 2; i++) {
+        const a = prompt('One of the latest watched movies?', ''),
+              b = prompt('What rank do you give that film?', '');
+    
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+            personalMovieDB.movies[a] = b;
+            console.log('done');
+        } else {
+            console.log('error');
+            i--; //Завдяки декріменту ми повертаємо користувача на крок назад
+        }
     }
 }
 
-if (personalMovieDB.count < 10) {
-    console.log("You watched too small amount of films");
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-    console.log("You are average watcher")
-} else if (personalMovieDB.count >= 30) {
-    console.log("You are a kinoman");
-} else {
-    console.log("Error")
+rememberMyFilms();
+
+function detectPersonalLevel() {
+    if (personalMovieDB.count < 10) {
+        console.log("You watched too small amount of films");
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+        console.log("You are average watcher")
+    } else if (personalMovieDB.count >= 30) {
+        console.log("You are a kinoman");
+    } else {
+        console.log("Error")
+    }
 }
+
+detectPersonalLevel();
 
 console.log(personalMovieDB);
